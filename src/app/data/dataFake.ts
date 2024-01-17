7c39e7d969d85a570c6c8cfe9313e4097d87e922
Luiz Marcelo <99672783+luizmipc@users.noreply.github.com>
2024-01-16 22:21:49 -0300

    Update dataFake.ts

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -2,13 +2,25 @@ export const dataFake = [
 	{
 		"id": "1",
 		"title": "NOVO HOMEN DE FERRO",
-		"description": "Marvel anuncia um novo filme do homem de ferro, confira",
+		"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
 		photoCover: "https://picfiles.alphacoders.com/177/177790.jpg"
 	},
 	{
 		"id": "2",
 		"title": "Nova Série Anunciada no Disney +",
-		"description": "bla bla bla",
+		"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
 		photoCover: "https://insidethemagic.net/wp-content/uploads/2021/01/Untitled-design-39-800x400.jpg"
+	},
+	{
+		"id": "3",
+		"title": "Novo filme do pantera negra lançado em breve",
+		"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+		photoCover: "https://i2.wp.com/blackpipe.com.br/wp-content/uploads/2018/02/Pantera-Negra-1.jpg?fit=900%2C500&ssl=1"
+	},
+	{
+		"id": "4",
+		"title": "X men vem para o MCU",
+		"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+		photoCover: "https://th.bing.com/th/id/R.c26bdb4e2864440856ccb22f002ecbcc?rik=KOCex3tTvwethA&riu=http%3a%2f%2fhdqwalls.com%2fwallpapers%2fx-men-the-last-stand-jd.jpg&ehk=%2byJ9od3Thg9k%2bneDBnLMk2VD8O9rpGRfFSZ7JAYrSa0%3d&risl=1&pid=ImgRaw&r=0"
 	},
 ]
